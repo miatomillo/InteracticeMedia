05deabbf81341b7ec88b896c6e4d52e4a0dfd962
Mia Tomillo <Mia@Mias-MacBook-Air.local>
2020-10-28 01:50:54 -0400

    done

--- game.js
+++ game.js
@@ -1,1 +1,33 @@
+d3.select("#color")
+    .on("click", function()
+       {
+      d3.select("body")
+        .append("div")
+        .text("click on a color")
+        } )
+
+d3.select("#yellow")
+ .on("click",function()
+    {
+      d3.select("body")
+        .append("p")
+        .text("Ability to Fly!")
+        } )
+
+d3.select("#blue")
+ .on("click",function()
+    {
+      d3.select("body")
+        .append("p")
+        .text("Ability to breathe underwater!")
+        } )
+
+d3.select("#red")
+ .on("click",function()
+    {
+      d3.select("body")
+        .append("p")
+        .text("Super Strength!")
+        } )
+
 
